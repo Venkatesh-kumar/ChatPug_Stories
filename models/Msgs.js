ea7e14b7ede20edc1a356142c10550bcf7f6d992
venkatesh kumar <venkateshkumar1405@gmail.com>
2023-09-03 16:29:33 +0000

    added timestamp for msgs

--- models/Msgs.js
+++ models/Msgs.js
@@ -5,7 +5,7 @@ const MsgsSchema = new Schema({
     from:{type: String},
     to:{type:String},
     msg:{type:String}
-})
+},{timestamps:true})
 
 const Msgs = mongoose.model('Msgs',MsgsSchema)
 module.exports = Msgs;
